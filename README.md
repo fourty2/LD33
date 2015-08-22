9b9a1fdff2adbc6aa131abe1ad9acf13dda5af2c
Marc Henklein <snooze82@gmail.com>
2015-08-22 14:12:25 +0200

    yeah, first platform

--- README.md
+++ README.md
@@ -1,3 +1,36 @@
-# LD33
+# LD33 - YOU ARE THE MONSTER
+
 My LudumDare #33 Compo Entry
+
+
+Hanni Ball 
+----------
+
+
+todos:
+ * Spielfeld
+ * spieler
+ * spieler steuerung (gamepad/s, tastatur)
+ 		vorwärts ist weg von camera, 
+ 		rückwärts zur camera hin
+ 		
+
+
+ * kollisionsabfrage (zum spielfeld und zum gegner)
+
+ * respawn
+
+
+* generierung von welten
+* spieler modellieren
+* bäume, etc. modellieren
+* musik/ soundeffekte
+
+* gimmicks
+
+
+* title screen 
+
+
+
 
---
+++ js/game.js
@@ -0,0 +1,86 @@
+var ld33 = {
+	renderer: null,
+	gamecanvas: null,
+	camera: null,
+	scene: null,
+	init: function() {
+	
+		var WIDTH = window.innerWidth;
+		var HEIGHT = window.innerHeight;
+
+		var NEAR = 0.1;
+		var FAR = 5000;
+		var FOV = 45;
+
+
+		this.gamecanvas = document.getElementById('gamecanvas');
+		this.renderer = new THREE.WebGLRenderer({canvas: this.gamecanvas});
+
+		this.renderer.setSize(WIDTH, HEIGHT);
+		this.renderer.shadowMap.enabled = true;
+		this.renderer.shadowMapSoft = true;
+		this.renderer.setClearColor(0xfff0f0, 1);
+
+
+		this.scene = new THREE.Scene();
+
+		this.camera = new THREE.PerspectiveCamera(FOV, WIDTH/ HEIGHT, NEAR, FAR);
+		this.camera.position.set(0,100,-100);
+		this.scene.add(this.camera);
+
+		// 
+		var light = new THREE.DirectionalLight(0xffffff);
+		light.position.set(0,1,0);
+		//light.target.position.set(0, 0, 0);
+		/*light.castShadow = true;
+		light.shadowCameraNear = 0.1;
+		light.shadowCameraFar = 300;
+
+		light.shadowCameraLeft = -50;
+light.shadowCameraRight = 50;
+light.shadowCameraTop = 50;
+light.shadowCameraBottom = -50;
+		light.shadowCameraVisible = true;
+		*/	
+		this.scene.add(light);
+
+
+
+                                                                                                                                                                                                                                                                          
+		var mesh = new THREE.Mesh(
+			new THREE.BoxGeometry(100,10,100),
+			new THREE.MeshLambertMaterial(
+				{color: 0x80f080,
+					shading: THREE.FlatShading
+				}));
+		this.scene.add(mesh);
+
+
+		console.log(this.camera.position);
+		this.camera.lookAt(mesh.position);
+
+
+
+
+		this.animate();
+		// @todo onresize handling
+	},
+
+	render: function() {
+		this.renderer.render(this.scene, this.camera);
+	},
+
+	animate: function() {
+		requestAnimationFrame(ld33.animate);
+		ld33.render()
+	},
+
+
+	// @todo later:
+	onResize: function() {
+
+	}
+};
+
+
+ld33.init();
